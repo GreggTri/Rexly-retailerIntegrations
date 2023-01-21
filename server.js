1f62f2e7e048d43b8b46ce474c69b8b7fd1f808c
Gregg Trimarchi <gttrimarchig@gmail.com>
2023-01-21 10:56:01 -0700

    deleted mongoose import, was causing error in prod
    don't have mongoose installed so no need

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 const logger = require('morgan')
 require('dotenv').config()
 
